1b9ac906ba4c8c3f058a4aad33eb24c319c30d4a
Ayesha <158054048+ShahAyesha@users.noreply.github.com>
2025-12-15 11:26:27 +0000

    Refactor SGD and SGD_momentum initialisations

--- optimisers.py
+++ optimisers.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 class SGD:
-    def __init__(self, params, lr=0.01):
-        self.params = params
+    def __init__(self, lr=0.01):
+        self.params = None
         self.lr = lr
 
     def step(self, grads):
 
         for i, loss_grad in enumerate(grads):
             self.params[i] -= self.lr * loss_grad
+            
             
     def zero_grad(self, grads):
         for i in range(len(grads)):
@@ -17,13 +18,16 @@ class SGD:
 
 
 class SGD_momentum:
-    def __init__(self, params, lr=0.01, beta=0.9):
-        self.params = params
+    def __init__(self, lr=0.01, beta=0.9):
+        self.params = None
         self.lr = lr
         self.beta = beta
-        self.momentums = [np.zeros_like(p) for p in params]
+        self.momentums = None
 
     def step(self, grads):
+        if self.momentums is None:
+            self.momentums = [np.zeros_like(g) for g in grads]
+            
         for i, loss_grad in enumerate(grads):
             self.momentums[i] = self.beta * self.momentums[i] + (1 - self.beta) * loss_grad
             self.params[i] -= self.lr * self.momentums[i]
